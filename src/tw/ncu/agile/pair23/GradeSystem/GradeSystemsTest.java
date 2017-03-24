608d5290b328bce12523559f4eda9a457bb40006
mepaling <dick50414@gmail.com>
2017-03-25 00:44:32 +0800

    add test class for gradesystemstest and gradestest

---
+++ src/tw/ncu/agile/pair23/GradeSystem/GradeSystemsTest.java
@@ -0,0 +1,54 @@
+package tw.ncu.agile.pair23.GradeSystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GradeSystemsTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGradeSystems() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowGrade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateWeights() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/tw/ncu/agile/pair23/GradeSystem/GradesTest.java
@@ -0,0 +1,39 @@
+package tw.ncu.agile.pair23.GradeSystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GradesTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGrades() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateTotalGrade() {
+		fail("Not yet implemented");
+	}
+
+}
