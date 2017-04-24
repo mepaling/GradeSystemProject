5b7040ffa5db0f3e954dc6e116a704c7e44b3258
Dan Shen <danny321974345@gmail.com>
2017-04-24 20:15:51 +0800

    add some pseudo codes

--- UI.java
+++ UI.java
@@ -1,80 +1,104 @@
 package tw.ncu.agile.pair23.GradeSystem;
 
-import java.util.Scanner;
-
 public class UI {
-	GradeSystems aGradeSystem;
-	Scanner scanner;
-	String id;
-	String command;
-	String StdName;
+/**
+***************************************************************************************************************
+class UI (user interface) 
 
-	public UI() {
-		aGradeSystem = new GradeSystems();
-		scanner = new Scanner(System.in);
-		while (true) {
-			promptID();
-			System.out.println(id);
-			if (id.equals("Q"))
-				break;
-			if (checkID(id) == false) {
-				// TODO
-				continue;
-			}
-			showWelcomeMsg();
-			while (true) {
-				boolean exit = false;
-				promptCommand();
-				switch (command) {
-				case "G":
-					// TODO
-					break;
-				case "R":
-					// TODO
-					break;
-				case "W":
-					// TODO
-					break;
-				case "E":
-					exit = true;
-					break;
-				default:
-					// TODO
-				}
-				if (exit)
-					break;
-			}
-		}
-		showFinishMsg();
-	}
+不斷prompt 使用者 ID. 直到使用者 quit.
+check 使用者 ID , 
+showWelcomeMsg
+    不斷prompt command. 做下列command直到使用者exit
+    showGrade, 
+    showRank, 
+    updateWeights 
+    exit
 
-	public boolean checkID(String id) {
-		if (aGradeSystem.containsID(id)) {
-			// TODO
-			return true;
-		} else
-			return false;
-	}
+checkID(ID)
+promptID()
+promptCommand()
+showFinishMsg()
+showWelcomeMsg()
+UI() 建構子 建構 aGradeSystem
+***************************************************************************************************************
+*/
 
-	public void promptID() {
-		System.out.println("輸入ID或 Q (結束使用)？");
-		id = scanner.nextLine();
-	}
+    public UI() {
+    /**
+    -----------------------------------------------------------------------------------------------------------
+    UI() 建構子 throws NoSuchIDExceptions, NoSuchCommandExceptions
+    -----------------------------------------------------------------------------------------------------------
+    */
+        int dummy=1; //the object is returned by calling this constructor for unit testing
+        try
+            1.call GradeSystems() to建構 aGradeSystem
 
-	public void promptCommand() {
-		System.out.println("輸入指令");
-		System.out.println("1) G 顯示成績 (Grade)");
-		System.out.println("2) R 顯示排名 (Rank)");
-		System.out.println("3) W 更新配分 (Weight)");
-		System.out.println("4) E 離開選單 (Exit)");
-		command = scanner.nextLine();
-	}
+            2.loop1 until Q (Quit)
+                  1. promptID() to get user ID  輸入ID或 Q (結束使用)？
+                  2. checkID (ID) 看 ID 是否在 aGradeSystem內
+                  3. showWelcomeMsg (ID)      ex. Welcome李威廷
+                  4. loop2 until E (Exit)
+                         promptCommand() to prompt for inputCommand
+                     end loop2
+              end loop1
 
-	public void showWelcomeMsg() {
-		System.out.println("Welcome");
-	}
+            3.showFinishMsg()           結束了
+        end try
+        finally {}
+    }
 
-	public void showFinishMsg() {
-		System.out.println("End");
-	}
+    public boolean checkID(String id) {
+    /**
+    -------------------------------------------------------------------------------------------------------------
+    checkID(ID) throws NoSuchIDExceptions return Boolean
+    parameter: ID   a user ID  ex: 123456789
+    time:     O(n)  n is  aGradeSystem 內全班人數
+    -------------------------------------------------------------------------------------------------------------
+    */
+        1. 要aGradeSystem 做containsID(ID) 看 ID 是否含在 aGradeSystem內
+        2. if not, throw an object of NoSuchIDExceptions
+        3. 回傳 true
+    }
+
+    public void promptID() {
+    /**
+    ------------------------------------------------------------------------------------------------------------
+    promptID()
+    ------------------------------------------------------------------------------------------------------------
+    */
+        1. prompt user for inputID
+        2. if input is "Q" then break
+           else assign inputID to userID end if
+        
+    }
+    public void promptCommand() {
+    /**
+    -------------------------------------------------------------------------------------------------------------
+    promptCommand() throws NoSuchCommandExceptions
+    -------------------------------------------------------------------------------------------------------------
+    */
+        1. prompt user for inputCommand
+        2. if inputCommand is not G (Grade),R (Rank), W (Weights), or E (Exit),
+           throws an object of NoSuchCommandException  end if
+        3. if inputCommand is E (Exit) then break
+           else: G aGradeSystem.showGrade(ID), R showRank(ID), W updateWeights() end if
+    }
+
+    public void showWelcomeMsg() {
+    /**
+    ------------------------------------------------------------------------------------------------------------
+    showWelcomeMsg()
+    ------------------------------------------------------------------------------------------------------------
+    */
+        輸出："Welcome "+ userName
+    }
+
+    public void showFinishMsg() {
+    /**
+    ------------------------------------------------------------------------------------------------------------
+    showFinishMsg()
+    ------------------------------------------------------------------------------------------------------------
+    */
+        輸出："結束了"
+    }
 }
